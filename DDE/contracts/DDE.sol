424d4a24134e59aec1b6efb38ccfc6015e11748b
merazzi <103759770+merazzi@users.noreply.github.com>
2024-03-31 20:20:00 +0200

    Update DDE.sol

--- DDE/contracts/DDE.sol
+++ DDE/contracts/DDE.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; //prova, voglio vedere quanto costa
 
 contract DDE{
-string public name = "Digital Deflazionitic Euro";
+string public name = "Digital deflationary Euro";
 string public symbol = "DDE";
 uint256 public totalSupply = 1000 * 5; // mille token con 5 decimali
 address public owner = 0x0d9E9930Df25C3Efe1042528F658d2C74856AeE8
@@ -26,6 +26,9 @@ constructor() { //ok
 	balanceOf[msg.sender] = totalSupply;
 	owner = msg.sender;
 	time = block.timestamp
+	year = 48 week
+	month = 30 day 
+	reserve = AggregatorV3Interface(0x0d9E9930Df25C3Efe1042528F658d2C74856AeE8);
 }
 
 function mint(address to, uint256 value) public { //only owner ok
@@ -44,12 +47,10 @@ function burn(address from, uint256 value) public { //only owner ok
 }
 
 function annualPrice() public{
-	//inserire l'anno anche per bisestile
-	annualOld = //controllo tempo da un anno all'altro
-	
-	if(time > 
+	dataOld = //
+	data = time
+	require(data == dataOld + year)
 	annualPrice = refencePrice;
-	
 }
 
 function InsertInflation(address to, uint256 value, uint _inputInflation) public{ 
@@ -63,13 +64,13 @@ function InsertInflation(address to, uint256 value, uint _inputInflation) public
 // Funzione per calcolare il nuovo prezzo di riferimento con l'inflazione mensile divisa in secondi per mese
 
 function updateReferencePrice() public {
-	// da mettere i giorni per mesi
+	// require la data precedente e controllare che sia giusta ovvero un mese
 	referencePriceOld = refencePrice;
 	referencePrice = annualPrice + inflation;
 	if (refencePriceOld =< referencePrice){ 
-	referencePrice = annualPrice + inflation / (giorni * 86.400) //aumenta il prezzo
+	referencePrice = annualPrice + inflation / ( g in mese * 86.400) //aumenta il prezzo
 	}else{ 
-	referencePrice = annualPrice - inflation /  (giorni * 86.400) // riduce il prezzo
+	referencePrice = annualPrice - inflation /  ( * 86.400) // riduce il prezzo
 	}
 }
 
@@ -78,13 +79,14 @@ function stabilityFee(){
 		fee = refencePrice - //acquisto * //amount
 		payable(owner).transfer(fee);
 	}else{
-		//avverto l'utente della transazione che più alta del 1%
-	
+		//avverto l'utente della transazione che più alta del 1% altrimenti fee
+		fee = refencePrice - //acquisto * //amount
+		payable(owner).transfer(fee);
 	}
 }
 
-function proofReserve(){
-
+function Reserve(){
+	//proof of reserve
 
 }
 
