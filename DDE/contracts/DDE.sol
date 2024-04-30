4bcb31dfeff10a08c51139da31803b43021539fb
merazzi <103759770+merazzi@users.noreply.github.com>
2024-04-30 11:21:14 +0200

    Update DDE.sol

--- DDE/contracts/DDE.sol
+++ DDE/contracts/DDE.sol
@@ -94,31 +94,5 @@ function stabilityFee(uint256 referencePrice, uint256 fee){ //da mettere nelle f
 		payable(owner).transfer(fee);
 	}
 	}
-
-*/
-
-function transfer(address to, uint256 value) external returns (bool) { 
-	require(balanceOf[msg.sender] >= value, "Insufficient balance");
-	balanceOf[msg.sender] -= value;
-	balanceOf[to] += value;
-	emit Transfer(msg.sender, to, value);
-	return true;
-}
-
-
-function approve(address spender, uint256 value) external returns (bool) { //ok
-	allowance[msg.sender][spender] = value;
-	emit Approval(msg.sender, spender, value);
-	return true;
-}
-
-function transferFrom(address from, address to, uint256 value) external returns (bool) { //ok
-	require(balanceOf[from] >= value, "Insufficient balance");
-	require(allowance[from][msg.sender] >= value, "Insufficient allowance");
-	balanceOf[from] -= value;
-	balanceOf[to] += value;
-	allowance[from][msg.sender] -= value;
-	emit Transfer(from, to, value);
-	return true;
-}
+/*
 }
