3bc583ca24ddc161566747885cf011598aced9f8
merazzi <103759770+merazzi@users.noreply.github.com>
2024-04-30 11:42:46 +0200

    Update manegement.sol

--- DDE/contracts/manegement.sol
+++ DDE/contracts/manegement.sol
@@ -5,14 +5,14 @@ address public owner = 0x0d9E9930Df25C3Efe1042528F658d2C74856AeE8;
 event Mint(address indexed to, uint256 value);
 event Burn(address indexed from, uint256 value); 
 
-function mint(address to, uint256 value) public { //only owner ok
+function mint(address to, uint256 value) public { 
 	require(msg.sender == owner);
 	balances[to] += value;
 	totalSupply += value;
 	emit Mint(to, value);
 }
 
-function burn(address from, uint256 value) public { //only owner ok
+function burn(address from, uint256 value) public {
 	require(msg.sender == owner);
 	require(balances[from] >= value, "Saldo insufficiente");
 	balances[from] -= value;
