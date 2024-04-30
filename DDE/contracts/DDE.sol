974ca1112c4e9ccc9ece0b7dbdeae8c9eeb6ba26
merazzi <103759770+merazzi@users.noreply.github.com>
2024-04-30 10:52:09 +0200

    Update DDE.sol
    i have create a management.sol for mint and burn

--- DDE/contracts/DDE.sol
+++ DDE/contracts/DDE.sol
@@ -121,19 +121,4 @@ function transferFrom(address from, address to, uint256 value) external returns 
 	emit Transfer(from, to, value);
 	return true;
 }
-
-function mint(address to, uint256 value) public { //only owner ok
-	require(msg.sender == owner);
-	balances[to] += value;
-	totalSupply += value;
-	emit Mint(to, value);
-}
-
-function burn(address from, uint256 value) public { //only owner ok
-	require(msg.sender == owner);
-	require(balances[from] >= value, "Saldo insufficiente");
-	balances[from] -= value;
-	totalSupply -= value;
-	emit Burn(from, value);
-}
 }
